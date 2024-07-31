2789f6c5d32bd60feb6eb0c33802ac4b847b0127
Eslam Osama <eslamebrahim1301@gmail.com>
2024-07-31 08:10:38 +0300

    updates

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -41,7 +41,6 @@ return {
         lualine_a = { "branch", diagnostics },
         lualine_b = { "mode" },
         lualine_c = {},
-        -- lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_x = { diff, spaces, "encoding", "filetype" },
         lualine_y = { "location" },
         lualine_z = { "progress" },
